7f24dddb182cbb01ad1469cd8499919ea9019993
Sultan <msultan.74u@gmail.com>
2026-03-04 14:01:22 +0200

    adding themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:sultan_portfolio/views/home_page.dart';
 
+/// لازم يكون خارج main عشان نقدر نستخدمه في أي مكان
+final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.dark);
+/// Global variables   ↑↑↑↑↑
+
+final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
 void main() {
-  runApp(const Sultan_Portfolio());
+  runApp(const SultanPortfolio());
 }
 
-class Sultan_Portfolio extends StatelessWidget {
-  const Sultan_Portfolio({super.key});
+class SultanPortfolio extends StatelessWidget {
+  const SultanPortfolio({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mohamed Sultan',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const HomePage(title: 'Mohamed Sultan'),
+    return ValueListenableBuilder(
+      valueListenable: themeNotifier,
+      builder: (context, ThemeMode currentMode, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Mohamed Sultan',
+
+          /// Light Theme
+          theme: ThemeData(
+            brightness: Brightness.light,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+
+          /// Dark Theme
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+              brightness: Brightness.dark,
+            ),
+            useMaterial3: true,
+          ),
+
+          /// الربط هنا مهم جدًا
+          themeMode: currentMode,
+
+          home: const HomePage(title: 'Mohamed Sultan'),
+        );
+      },
     );
   }
 }
